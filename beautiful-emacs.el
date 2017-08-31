8fd664eb20300d4424fcd4231777ac578ccf0f57
jack <jacktasia@gmail.com>
2017-08-31 02:03:30 +0000

    add built-in config

--- beautiful-emacs.el
+++ beautiful-emacs.el
@@ -1,4 +1,5 @@
-(setq make-backup-files nil)       ;; no backup files!
+
+
 
 ;; bootstrap straight.el package manager
 (let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
@@ -17,12 +18,65 @@
 
 (use-package dumb-jump :ensure t)
 
-(use-package zerodark-theme 
-  :ensure t)
+(use-package zerodark-theme :ensure t)
 
+;; TODO: add all packages
+;; TODO: add all packages config
+;; TODO: add all built-in keybindings...
+
+;;
+;; start built-in config changes
+;;
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
+(setq uniquify-min-dir-content 3)
+;; https://github.com/emacs-mirror/emacs/blob/0537943561a37b54467bec19d1b8afbeba8e1e58/lisp/uniquify.el#L107
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)                ;; or "forward"
+(setq tramp-default-method "scpx")
+(setq clean-buffer-list-delay-general 7)
+(show-paren-mode t)
+(add-to-list 'auto-mode-alist '("\\.el\\'" . emacs-lisp-mode))
+(setq org-log-done t)                                                         ;; show done time when marking a todo done
+(defalias 'yes-or-no-p 'y-or-n-p)                                             ;; don't require full "yes" for confirms
+(tool-bar-mode -1)                                                            ;; get rid of tool bar
+(setq inhibit-startup-message t)                                              ;; git rid of startup page
+(menu-bar-mode 0)                                                             ;; no menu bar
+(setq resize-mini-windows t)                                                  ;; let mini buffer resize
+(setq make-backup-files nil)                                                  ;; no backup files
+(setq-default c-electric-flag nil)                                            ;; do not get fancy with () {} ?
+(setq whitespace-line-column 60000)                                           ;; do not turn line purple if "too long"
+(blink-cursor-mode 0)                                                         ;; no blinking cursor
+(setq initial-scratch-message "")                                             ;; no scratch message
+(electric-indent-mode 0)                                                      ;; stop electric mode from being too smart for its own good
+(global-hl-line-mode 1)
+;; (global-auto-revert-mode 1)                                                ;; so git branch changes and checkouts update the mode line (slow, so disabled)
+(setq auto-revert-check-vc-info nil)
+(setq confirm-kill-emacs 'y-or-n-p)
+(setq message-log-max t)                                                      ;; If t, log messages but don't truncate the buffer when it becomes large.
+(setq-default cursor-in-non-selected-windows nil)                             ;;
+(setq column-number-mode t)                                                   ;;
+(global-subword-mode t)                                                       ;; 
+
+
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+(setq create-lockfiles nil)
+(setq cua-enable-cua-keys nil)
+(cua-mode)
+(if window-system
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+;; http://stackoverflow.com/a/25438277/24998
+(global-linum-mode)
 
 (defun display-startup-echo-area-message ()
   (message "~/.emacs loaded in %s!" (emacs-init-time)))
+
+;;
+;; end built-in config changes
+;;
+
+
+
+
 
 
 
