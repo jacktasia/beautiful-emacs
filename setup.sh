048dd6f968bf508e9e1c91b4340ca8fa1e122028
jack <jacktasia@gmail.com>
2017-09-03 22:04:33 +0000

    update setup.sh to have .emacs load via org

--- setup.sh
+++ setup.sh
@@ -3,11 +3,18 @@ set -e
 
 INIT_ORG="${HOME}/.emacs.d/init.org"
 INIT_EL="${HOME}/.emacs.d/init.el"
+DOT_EMACS="${HOME}/.emacs"
 
 if [ -e $INIT_ORG ]
 then
-    echo 'Removing existing init.org symlink.'
-    rm $INIT_ORG
+    echo -e "\n\nBacking up existing ~/.emacs.d/init.org"
+    mv $INIT_ORG  $INIT_ORG.bbkup
+fi
+
+if [ -e $DOT_EMACS ]
+then
+    echo -e "\n\nBacking up ~/.emacs"
+    mv $DOT_EMACS $DOT_EMACS.bbkup
 fi
 
 CUR_DIR=$(pwd)
@@ -23,7 +30,12 @@ then
     ln -s $BEAUTIFUL_INIT_ORG $INIT_ORG
 fi
 
+
+
 echo -e "\n\nConverting ~/.emacs.d/init.org -> ~/.emacs.d/init.el -> LOADING..."
-emacs --batch --eval "(progn (require 'org) (org-babel-tangle-file \"${INIT_ORG}\") (load-file \"${INIT_EL}\"))"
+emacs --batch --eval "(progn (require 'org) (org-babel-load-file \"${INIT_ORG}\"))"
+
+# set ~/.emacs content to load from the org file
+echo -e "(require 'org)\n(org-babel-load-file \"${INIT_ORG}\")\n;; Edit the beautiful-emacs/init.org file instead!" > $DOT_EMACS
 
 echo -e "\n\nDone."
