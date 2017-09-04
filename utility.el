e531f26f109372140f491b8dd3d8963efe1cf6a7
jack <jacktasia@gmail.com>
2017-09-04 03:33:08 +0000

    add autoload to utilities

--- utility.el
+++ utility.el
@@ -1,9 +1,12 @@
+
+;;;###autoload
 (defun beautiful-backward-delete-word (arg)
   "Delete characters backward until encountering the beginning of a word.
 With argument ARG, do this that many times. Does NOT add to kill ring."
   (interactive "p")
   (delete-region (point) (progn (backward-word arg) (point))))
 
+;;;###autoload
 (defun beautiful-helm-projectile-ag-at-point ()
   (interactive)
   (let ((helm-ag-insert-at-point 'symbol))
